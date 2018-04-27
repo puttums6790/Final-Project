cd09a448792871821719f7cfdfb21b34545a179b
Jack Kennedy <puttums6790@gmail.com>
2018-04-27 17:30:55 -0500

    added google login

--- rentalapp/src/App.js
+++ rentalapp/src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import GoogleLogin from 'react-google-login';
+
+ 
+ 
+const responseGoogle = (response) => {
+  console.log(response);
+}
+ 
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <GoogleLogin
+    clientId="1031979254140-uf8nemp2p36c7jn86am3alehkt0sl74i.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+      />
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
