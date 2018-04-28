91b7717f5bf4dff77dd3424f1ca4e64af18105a2
Noah Strayve <noahstrayve@gmail.com>
2018-04-28 12:54:34 -0500

    have to push image to render

--- src/App.js
+++ src/App.js
@@ -2,18 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function UserNavbar(props) {
+  return <div className="header">
+          <div className="titleBox">
+            <h1>ReLacks</h1>
+          </div>
+          <div className="routesBox">
+            <div className="routesHolder">
+              <p>Example</p>
+            </div>
+            <div className="routesHolder">
+              <p>Example</p>
+            </div>
+            <div className="routesHolder">
+              <p>Example</p>
+            </div>
+          </div>
+        </div>;
+}
+
+function GuestNavbar(props) {
+  return <div className="header">
+          <div className="titleBox">
+            <h1>ReLacks</h1>
+          </div>
+          <div className="routesBox">
+            <div className="routesHolder">
+              <p>SIGN IN</p>
+            </div>
+            <div className="routesHolder">
+              <p>NOT A MEMBER? REGISTER</p>
+            </div>
+          </div>
+        </div>;
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <GuestNavbar />
     );
   }
 }
