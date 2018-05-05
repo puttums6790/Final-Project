e287959aaf76d82ae7c2f3f57e6de9680ac68810
ajmaravilla <john.maravilla@me.com>
2018-05-05 11:45:54 -0500

    updated wording

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -17,16 +17,18 @@ const Landing = pages => {
       <div id="firstBlurbBox">
     <div className="firstBlurbOne">
       <div className="firstBlurbOneTitle">
-        <h1>HOOCHIE MAMA</h1>
+        <h1>Give Stuff</h1>
       </div>
       <div className="firstBlurbOneText">
-        <p>scudfhaefuhaf afhafou afuoghf ahfuhaof hawu af af  aefaef af ea aef aef afgaegf</p>
-        <p>scudfhaefuhaf afhafouas sada fe gsgdagf sdgsf gs dgs  gssdgsdg sdg sdg sdg sdg sdg sdg afuoghf ahfuhaof hawu af af  aefaef af ea aef aef afgaegf</p>
+        <p>Don't toss your stuff if you no longer need them!</p>
+        <p>Give your stuff away to someone else who may get some good use from them. As the saying goes "One man's trash is another man's treasure"! </p>
+        <p>Plus you will be helping the environment by promoting the reuse of your items.</p>
+        <p>On a vacation at a beach resort and you purchased boogie boards, beach chairs, and pool noodles for your stay, but you don't want to take it back home with you on the plane? Give it to someone on the resort or in the area who needs it.</p>
       </div>
     </div>
     <div className="firstBlurbTwo">
       <div className="firstBlurbTwoTitle">
-        <h1>HOOCHIE MAMA</h1>
+        <h1>Get Stuff</h1>
       </div>
       <div className="firstBlurbTwoText">
         <p>scudfhaefuhaf afhafou afuoghf ahfuhaof hawu af af  aefaef af ea aef aef afgaegf</p>
