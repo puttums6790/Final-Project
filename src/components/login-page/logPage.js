8ebe129e1677993074a1a8b99470bb5fc740d201
Noah Strayve <noahstrayve@gmail.com>
2018-05-07 21:01:38 -0500

    fixed footer css

--- src/components/login-page/logPage.js
+++ src/components/login-page/logPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './LogPage.css'
 import './hikingImageTwo.jpg'
+import LogIn from '../../clickHandlers/logIn'
 
 const LoginPage = pages => {
   return <div className="registrationFrame">
@@ -13,8 +14,8 @@ const LoginPage = pages => {
           <label for="passwordRegistrationField">Password</label>
           <input type="password" className="form-control" id="passwordRegistrationField" placeholder="Password"/>
         </div>
-        <div className="registrationPageButtonBox">
-        <h6 class="registrationPageButtonText">Submit</h6>
+        <div className="registrationPageButtonBox" onClick={LogIn}>
+        <h6 className="registrationPageButtonText">Submit</h6>
       </div>
         
 
