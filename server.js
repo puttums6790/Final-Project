7fa3d6bcc5c3a8d8703bdd7016f817fcb6716cd5
Jack Kennedy <puttums6790@gmail.com>
2018-05-10 17:20:26 -0500

    working mongodb backend

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-‘use strict’
+'use strict'
 
 //first we import our dependencies…
-var express = require(‘express’);
-var mongoose = require(‘mongoose’);
-var passport = require('passport');
+var express = require('express');
+var mongoose = require('mongoose');
+//var passport = require('passport');
+var api = require('./server/routes/api/api')
 
-var bodyParser = require(‘body-parser’);
+var bodyParser = require('body-parser');
 
 //and create our instances
 var app = express();
@@ -21,29 +22,23 @@ app.use(express.static("client/build"));
 //To prevent errors from Cross Origin Resource Sharing, we will set 
 //our headers to allow CORS with middleware like so:
 app.use(function(req, res, next) {
- res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
- res.setHeader(‘Access-Control-Allow-Credentials’, ‘true’);
- res.setHeader(‘Access-Control-Allow-Methods’, ‘GET,HEAD,OPTIONS,POST,PUT,DELETE’);
- res.setHeader(‘Access-Control-Allow-Headers’, ‘Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers’);
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 //and remove cacheing so we get the most recent comments
- res.setHeader(‘Cache-Control’, ‘no-cache’);
+ res.setHeader('Cache-Control', 'no-cache');
  next();
 });
 //now we can set the route path & initialize the API
-router.get(‘/’, function(req, res) {
- res.json({ message: ‘API Initialized!’});
+router.get('/', function(req, res) {
+ res.json({ message: 'API Initialized!'});
 });
 //Use our router configuration when we call /api
-app.use(‘/api’, router);
+api(router)
+app.use('/api', router);
 
-mongoose.Promise = global.Promise;
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/nytarticles",
-  {
-    useMongoClient: true
-  }
-);
+//
 
 //starts the server and listens for requests
 app.listen(port, function() {
